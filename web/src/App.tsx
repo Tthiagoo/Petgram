890e73bfd31952cb3f9f086df2653f377fdc6f7b
Tthiagoo <51219408+Tthiagoo@users.noreply.github.com>
2020-10-20 17:01:41 -0300

    update

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,7 +6,6 @@ import ThemeContainer from "./Theme/ThemeContainer";
 
 function App() {
  
-  
   return (
     <div className="App">
       <ThemeContainer>
--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,25 +1,47 @@
 import React from "react";
 import ThemeSelector from "../../components/ThemeChange";
-import { Heading, Grid, Flex, Link, Button, Text, useColorMode } from "@chakra-ui/core";
+import {
+	Heading,
+	Grid,
+	Flex,
+	Link,
+	Button,
+	Text,
+	useColorMode,
+} from "@chakra-ui/core";
+
+import Input from "../../components/Input";
+import Divider from "../../components/Divider";
 
 export default function Login() {
-	const { colorMode} = useColorMode();
+	const { colorMode } = useColorMode();
 
 	return (
 		<Grid
-    
 			as="main"
 			height="100vh"
-			templateColumns={['1fr','1fr 1fr']}
-      templateRows={["1fr 614px 1fr","185px 1fr 185px"]}
-      templateAreas={["'.' 'logo''.'", "'. .' 'form logo' '. .'"]}
+		
+			templateColumns={[
+				"1fr", 
+				"1fr 1fr"]}
+
+			templateRows={[
+				"1fr 614px 1fr", 
+				"185px 1fr 185px"]}
+
+			templateAreas={["'.' 'form''.'", "'. .' 'form logo' '. .'"]}
 			justifyContent="center"
 			alignItems="center"
-			backgroundImage={colorMode === "light" ? "linear-gradient(to right top, #95c9ff, #95beeb, #94b4d7, #92a9c3, #909fb0);" : "linear-gradient(to right top, #445e7a, #365170, #284466, #1a375b, #092b51)"}
+			
+			backgroundImage={
+				colorMode === "light"
+					? "linear-gradient(to right top, #95c9ff, #95beeb, #94b4d7, #92a9c3, #909fb0);"
+					: "linear-gradient(to right top, #445e7a, #365170, #284466, #1a375b, #092b51)"
+			}
 		>
-			<ThemeSelector
-				aria-label=""
-			/>
+			<ThemeSelector aria-label="" gridArea="form" />
+
+			
 		</Grid>
 	);
 }
