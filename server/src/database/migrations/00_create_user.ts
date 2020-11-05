c2bc04b3e6574d38067e45ba0765932f16bfd37d
Thiago Pereira <51219408+Tthiagoo@users.noreply.github.com>
2020-11-04 22:44:12 -0300

    Update Create user

--- server/src/database/migrations/00_create_user.ts
+++ server/src/database/migrations/00_create_user.ts
@@ -3,6 +3,10 @@ import Knex from 'knex';
 export async function up(knex:Knex){
     knex.schema.createTable('users',table=>{
         table.increments('id').primary();
-        table.string('')
+        table.string('name',25).notNullable()
+        table.string('username',12).unique().notNullable()
+        table.integer('password').notNullable()
+        table.text('bio')
+        
     })
 }
