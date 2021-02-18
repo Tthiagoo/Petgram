a6243eedfab9af425fdae69c8d65866c704b78f0
Tthiagoo <51219408+Tthiagoo@users.noreply.github.com>
2021-02-18 17:03:11 -0300

    update

--- web/src/pages/Main/index.tsx
+++ web/src/pages/Main/index.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useEffect, useState } from "react";
 
-import { Grid, Flex, useColorMode } from "@chakra-ui/core";
+import { Grid, Flex, useColorMode, Box } from "@chakra-ui/core";
 
 import { List } from "@chakra-ui/core";
 
@@ -37,7 +37,6 @@ export default function Main() {
 			height="100vh"
 			alignItems="center"
 			justifyContent="center"
-			
 			backgroundImage={
 				colorMode === "light"
 					? "linear-gradient(to left bottom, #a6bbcd, #aecbd2, #c0d8d5, #d6e4da, #edf0e5);"
@@ -56,23 +55,22 @@ export default function Main() {
 				<Grid
 					gridArea="feed"
 					templateRows="1fr"
-					templateColumns={["1fr", "1fr", "1fr", "1fr 40%", "1fr 30%"]}
+					templateColumns={["1fr", "1fr", "1fr", "1fr 40%", "1fr 40%"]}
 					gridTemplateAreas={["posts", "posts", "posts", "'posts' 'info'"]}
 					width="100%"
 					height="100%"
+					overflowX="hidden"
+					overflowY="scroll"
 				>
 					<List
 						display="flex"
 						width="100%"
-						height="100%"
-						maxHeight="100%"
-						overflowX="hidden"
-						overflowY="scroll"
+						height="auto"
+					
 						flexDirection="column"
-						alignItems="center"
+						alignItems={["center","center","center","flex-end"]}
 						gridArea="posts"
 						style={{ WebkitScrollSnapType: "none" }}
-						
 					>
 						{posts.map((post, index) => (
 							<Post
@@ -85,6 +83,24 @@ export default function Main() {
 							/>
 						))}
 					</List>
+					
+					<Flex
+					display={["none","none","none","flex"]}
+						width="100%"
+						height="100%"
+						justifyContent="start"
+						
+					>
+						<Box
+						
+							width="30%"
+							height="30%"
+							border="1px solid yellow"
+							position="fixed"
+							marginTop="20px"
+							marginLeft="10px"
+						></Box>
+					</Flex>
 				</Grid>
 			</Grid>
 		</Flex>
